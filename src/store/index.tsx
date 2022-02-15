fa3ca6ee4169f2779210fe7df2b40918ac5e3934
Mikle_D <mda@atticlab.net>
2022-02-15 10:27:51 +0200

    rewrite store(don't use reducer)

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,34 +1,14 @@
-import React, { createContext, FC, useMemo, useReducer } from 'react';
+import React, { createContext, FC, useMemo, useState } from 'react';
 import { CHAIN_LIST_MAINNET } from '../utils/constants';
 import { chooseAccount } from './methods/chooseAccount';
 import { IChainList } from '../interface/ChainList';
-
-type ActionType = {
-    type: string;
-    payload: any;
-};
 
 interface IInitialState {
     chain?: IChainList;
     account?: any;
 }
 
-const initialState: IInitialState = {
-    // chain: CHAIN_LIST_MAINNET[0],
-};
-
-const reducer = (state = initialState, action: ActionType) => {
-    switch (action.type) {
-        case 'SET_CHAIN':
-            return { ...state, chain: action.payload };
-        case 'SET_ACCOUNT':
-            return { ...state, account: action.payload };
-        case 'SET_BALANCE':
-            return { ...state, balance: action.payload };
-        default:
-            return state;
-    }
-};
+const initialState: IInitialState = {};
 
 const store = createContext<any>({
     state: initialState,
@@ -37,27 +17,30 @@ const store = createContext<any>({
 const { Provider } = store;
 
 const StoreProvider: FC = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [chain, setChain] = useState<IChainList>();
+    const [account, setAcc] = useState();
+    const [balance, setBal] = useState();
 
     const setBalance = async (
         address: string,
         handleBalance: (address: string) => Promise<any>,
     ) => {
-        const bal = await handleBalance(address);
+        try {
+            const bal = await handleBalance(address);
 
-        dispatch({
-            type: 'SET_BALANCE',
-            payload: bal.data.result[0],
-        });
+            setBal(bal.data.result[0]);
+        } catch (e: any) {
+            console.error(e);
+        }
     };
 
     const setAccount = async (chain: IChainList) => {
         localStorage.setItem('chain', JSON.stringify(chain));
 
-        dispatch({ type: 'SET_CHAIN', payload: chain });
+        setChain(chain);
 
         const account = await chooseAccount(chain);
-        dispatch({ type: 'SET_ACCOUNT', payload: account });
+        setAcc(account);
     };
 
     window.onload = async () => {
@@ -72,12 +55,13 @@ const StoreProvider: FC = ({ children }) => {
 
     const provider = useMemo(() => {
         return {
-            ...state,
-            dispatch,
             setAccount,
             setBalance,
+            chain,
+            account,
+            balance,
         };
-    }, [state]);
+    }, [chain, account, balance]);
 
     return <Provider value={provider}>{children}</Provider>;
 };
